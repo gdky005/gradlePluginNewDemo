db98da62148538c4f77cce8a6802139dde6f0570
qingw <qingw@jumei.com>
2017-07-06 19:03:10 +0800

    添加 第二个 自定义 gradle demo 的插件。

---
+++ buildsrc/src/main/groovy/wq/gdky005/com/MyTransform.groovy
@@ -0,0 +1,77 @@
+package wq.gdky005.com
+
+import com.android.build.api.transform.*
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
+import org.gradle.api.Project
+
+class MyTransform extends Transform {
+
+    Project project
+
+    MyTransform(Project project) {
+        this.project = project
+    }
+
+    @Override
+    String getName() {
+        return '-->WQTransform<--'
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    @Override
+    void transform(Context context, Collection<TransformInput> inputs, Collection<TransformInput> referencedInputs, TransformOutputProvider outputProvider, boolean isIncremental) throws IOException, TransformException, InterruptedException {
+        println "=============hello, body!=============="
+
+        //Transform的inputs有两种类型，一种是目录，一种是jar包，要分开遍历
+        inputs.each { TransformInput input ->
+            //对类型为“文件夹”的input进行遍历
+            input.directoryInputs.each { DirectoryInput directoryInput ->
+                //文件夹里面包含的是我们手写的类以及R.class、BuildConfig.class以及R$XXX.class等
+
+
+                MyInject.injectDir(directoryInput.file.absolutePath, "gdky005.timer")
+                // 获取output目录
+                def dest = outputProvider.getContentLocation(directoryInput.name,
+                        directoryInput.contentTypes, directoryInput.scopes,
+                        Format.DIRECTORY)
+
+                // 将input的目录复制到output指定目录
+                FileUtils.copyDirectory(directoryInput.file, dest)
+            }
+
+            //对类型为jar文件的input进行遍历
+            input.jarInputs.each { JarInput jarInput ->
+                //jar文件一般是第三方依赖库jar文件
+
+                // 重命名输出文件（同目录copyFile会冲突）
+                def jarName = jarInput.name
+                def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsoluteFile())
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length() - 4)
+                }
+
+                //生成输出路径
+                def dest = outputProvider.getContentLocation(jarName + md5Name,
+                        jarInput.contentTypes, jarInput.scopes, Format.JAR)
+
+                //将输入内容复制到输出
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
+    }
+}
